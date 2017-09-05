1f0dd4a86df0896214809602b4f90b32b0e1aaa1
Renata Rodrigues <ticare1@hotmail.com>
2017-09-05 19:15:53 -0300

    Criamos a classe Usuário

--- class/Sql.php
+++ class/Sql.php
@@ -7,14 +7,14 @@ class Sql extends PDO {
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$statment->bindParam($key, $value);
+			$statement->bindParam($key, $value);
 		}
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 
 	//$rawQuery - comando sql que será tratado
